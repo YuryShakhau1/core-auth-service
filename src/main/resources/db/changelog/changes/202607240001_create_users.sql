095b435d11f761ac7987766b238bfdc4ac6e94f2
Yury Shakhau <shakhauyury@gmail.com>
2026-07-24 15:08:06 +0300

    Db schema implementation

---
+++ src/main/resources/db/changelog/changes/202607240001_create_users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users(
+    id          UUID PRIMARY KEY,
+    first_name  VARCHAR(50) NOT NULL,
+    last_name   VARCHAR(50) NOT NULL,
+    email       VARCHAR(100) NOT NULL UNIQUE,
+    password    VARCHAR(100) NOT NULL,
+    active      BOOLEAN NOT NULL,
+    created_at  TIMESTAMP NOT NULL,
+    updated_at  TIMESTAMP NOT NULL
+);
